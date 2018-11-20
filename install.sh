49543d1978cad31d4a2ff0ad6d087c2f3bafa690
laurent.f.george@gmail.com <laurent.f.george@gmail.com>
2018-11-20 19:52:41 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 # /bin/bash
-docker pull laurentgeorge3/ros-esme:full
+#docker pull laurentgeorge3/ros-esme:full
+
+# installing directly on the machine
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list' 
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add - 
+
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full -y
