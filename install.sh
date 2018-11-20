474cb703a6236d0e22794ab7365b742a623bff24
laurent.f.george@gmail.com <laurent.f.george@gmail.com>
2018-11-20 20:47:51 +0100

    update install script to get turtlebot gazebo

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo 
 
 sudo apt-get update
 sudo apt-get install ros-kinetic-desktop-full -y
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main"
+sudo apt-get update
+sudo apt-get install librealsense2-dkms ros-kinetic-librealsense linux-headers-generic -y
+sudo apt-get install ros-kinetic-turtlebot-gazebo -y
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
